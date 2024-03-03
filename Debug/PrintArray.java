eb208d5f19e259e019c3297056551006c56429fd
disha <dishatimbadiya055@gmail.com>
2024-03-03 11:36:10 +0530

    print functions added

--- Debug/PrintArray.java
+++ Debug/PrintArray.java
@@ -1,16 +1,21 @@
-public static void print1D(int arr[]) {
+public static void print1D(int arr[]) 
+{
     int n = arr.length;
-    for (int i = 0; i < n; i++) {
+    for (int i=0;i<n; i++) 
+    {
         System.out.print(arr[i] + " ");
     }
     System.out.println("");
 }
-public static void print2D(int arr[][]) {
+public static void print2D(int arr[][]) 
+{
     int n = arr.length;
-    int m = arr[0].length;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            System.err.print(arr[i] + " ");
+    int m=arr[0].length;
+    for (int i=0;i<n; i++) 
+    {
+        for(int j=0;j<m;j++)
+        {
+            System.err.print(arr[i]+" ");
         }
         System.out.println("");
     }
